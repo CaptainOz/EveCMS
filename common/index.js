082f7469674b7ca7038709a99658d1342230cc4c
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-10 23:01:18 -0700

    Added server-only index file

---
+++ common/index.js
@@ -0,0 +1,10 @@
+
+// Only set up module exports if we are on the server. These symbols aren't defined on the client.
+if( typeof module   == 'object'     &&
+    typeof exports  == 'object'     &&
+    typeof require  == 'function'
+){
+    module.exports = {
+        'events' : require( './events.js' )
+    };
+}
